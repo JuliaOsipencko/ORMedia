9ae44ba8c1f98218e793bcde84615349a7e7fb7b
JuliaOsipencko <47526066+JuliaOsipencko@users.noreply.github.com>
2019-05-06 15:55:33 +0300

    Delete js.js

--- HW1/task1/js.js
+++
@@ -1,7 +0,0 @@
-var NameUser = prompt('Введите ваше имя пользователя!', '');
-if (/[0-9]/.test(NameUser)) {
-    document.write(NameUser.toUpperCase());
-}
-else {
-    document.write(NameUser.split('').reverse().join(''));
-}
