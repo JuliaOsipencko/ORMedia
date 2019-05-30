c9de1d5569f867adcb50383b5c2c6e57131ec093
JuliaOsipencko <47526066+JuliaOsipencko@users.noreply.github.com>
2019-05-30 14:24:26 +0300

    fix: added event handler

--- HW1/task2/js.js
+++ HW1/task2/js.js
@@ -1,3 +1,3 @@
-function drawLayout() {
+draw.onclick = function () {
     document.body.innerHTML = '<iframe name="iframe" src="newIndex.html" style="width: 100%; height: 630px;" frameborder="0"></iframe>';
-}
+};
